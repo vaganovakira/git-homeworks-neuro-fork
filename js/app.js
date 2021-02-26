78ecc9d37750dd385eaebc67ab781df5deaed7e0
vaganovakira <76817816+vaganovakira@users.noreply.github.com>
2021-02-26 14:25:36 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 (async () => {
- HEAD
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             const coordsEl = document.querySelector('#user-coords');
@@ -9,5 +8,4 @@
     }
 
     // TODO: add custom logic here
- 857e3d95477bfbd9c93fb04804a64d0007c1c075
 })();
